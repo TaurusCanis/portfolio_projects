ee5539b0620c4ca9e919bf07b558241a2e470845
Taurus Canis <acdole09@gmail.com>
2022-10-16 13:49:49 -0400

    added models to admin.py

--- mbsr/admin.py
+++ mbsr/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import FormalPracticePromptInfo, InformalPracticeInfo, MBSRUser, GettingStartedResponse, FormalPractice, InformalPractice
 
-# Register your models here.
+admin.site.register(MBSRUser)
+admin.site.register(FormalPracticePromptInfo)
+admin.site.register(InformalPracticeInfo)
+admin.site.register(GettingStartedResponse)
+admin.site.register(FormalPractice)
+admin.site.register(InformalPractice)
