0d84801cc2e02a9a0b937d61d12cdd4b72ca4e58
Taurus Canis <acdole09@gmail.com>
2022-10-26 18:41:58 -0400

    changed stripe idempot id process

--- ecommerce_backend/views.py
+++ ecommerce_backend/views.py
@@ -123,7 +123,7 @@ class StripeWebhook(APIView):
         return
 
     def update_order_details(self, event):
-        order = self.get_order(event['data']['objet']['id'])[0]
+        order = self.get_order(event['data']['object']['id'])[0]
         order.update(
             ref_code = self.create_confirmation_number(),
             ordered = True,
